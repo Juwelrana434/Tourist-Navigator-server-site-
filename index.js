9200ee5a9340bb5a20113e51a68a8476765daf45
Juwelrana434 <jewel.rana434@gmail.com>
2024-06-11 23:36:16 +0600

    admin add read

--- index.js
+++ index.js
@@ -126,7 +126,19 @@ app.get('/users', async(req, res) => {
   const result = await cursor.toArray();
   res.send(result);
   });
-
+  
+ // make admin user
+ app.patch('/users/admin/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      role: 'admin'
+    }
+  }
+  const result = await usersCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+})
 
 // Verify admin 
 app.get('/users/admin/:email', async (req, res) => {
@@ -137,7 +149,7 @@ app.get('/users/admin/:email', async (req, res) => {
   }
 
   const query = { email: email };
-  const user = await userCollection.findOne(query);
+  const user = await usersCollection.findOne(query);
   let admin = false;
   if (user) {
     admin = user?.role === 'admin';
